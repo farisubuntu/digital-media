3acffc8e41a4c957ce22e464a87aa41c95801a37
farisubuntu <farisubuntu@gmail.com>
2024-06-30 19:22:02 +0300

    asedf

--- src/ui/dashboard/StatCard/StatCard.jsx
+++ src/ui/dashboard/StatCard/StatCard.jsx
@@ -8,6 +8,13 @@ export default function InfoCard({ iconSrc, title, value, link }) {
       href={link}
       className="flex flex-col border rounded-lg shadow mx-2 my-2 hover:bg-blue-700 hover:text-white transition-colors duration-200"
     >
+      <Image
+        src={iconSrc}
+        alt="Icon"
+        width={50}
+        height={50}
+        className="w-12 h-12 mx-auto my-4 bg-inverted"
+      />
       <dd className="order-first text-3xl font-semibold tracking-tight text-white">
         {title}
       </dd>
