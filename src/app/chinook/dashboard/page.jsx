c525190491063e26530ac6e386081f7fb27ea05e
farisubuntu <farisubuntu@gmail.com>
2024-06-30 19:37:10 +0300

    asdf

--- src/app/chinook/dashboard/page.jsx
+++ src/app/chinook/dashboard/page.jsx
@@ -63,14 +63,16 @@ const cardsInfo = [
 
 export default async function DashboardHomePage() {
   return (
-    <div className="bg-gray-900 rounded-lg mx-1">
-      <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <div className="mx-auto max-w-2xl lg:max-w-none">
-          <dl className="mt-16 grid grid-cols-1 gap-0.5 overflow-hidden rounded-2xl text-center sm:grid-cols-2 lg:grid-cols-4">
-            {cardsInfo.map((card, index) => (
-              <StatCard key={index} {...card} />
-            ))}
-          </dl>
+    <div className="w-full h-full">
+      <div className="bg-gray-900 rounded-lg mx-1">
+        <div className="mx-auto max-w-7xl px-6 lg:px-8">
+          <div className="mx-auto max-w-2xl lg:max-w-none">
+            <dl className="mt-16 grid grid-cols-1 gap-0.5 overflow-hidden rounded-2xl text-center sm:grid-cols-2 lg:grid-cols-4">
+              {cardsInfo.map((card, index) => (
+                <StatCard key={index} {...card} />
+              ))}
+            </dl>
+          </div>
         </div>
       </div>
     </div>
