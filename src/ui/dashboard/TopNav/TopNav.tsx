e0f176c15a89c611393b9ffc1598a103e0e5fe84
farisubuntu <farisubuntu@gmail.com>
2024-07-04 22:58:55 +0300

    asdf

--- src/ui/dashboard/TopNav/TopNav.tsx
+++ src/ui/dashboard/TopNav/TopNav.tsx
@@ -181,10 +181,12 @@ export default function TopNav() {
                 <div className="border-t border-gray-700 pb-3 pt-4">
                   <div className="flex items-center px-5">
                     <div className="flex-shrink-0">
-                      <img
+                      <Image
                         className="h-10 w-10 rounded-full"
                         src={user.imageUrl}
                         alt=""
+                        width={32}
+                        height={32}
                       />
                     </div>
                     <div className="ml-3">
