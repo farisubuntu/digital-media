38b30c6f2067d4ffb5b6d286ed0486f29c1267b2
farisubuntu <farisubuntu@gmail.com>
2024-07-13 00:56:54 +0300

    asdf

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,70 @@
-import { StatCardInterface } from "@/lib/definitiions";
-export default function DashboardPage() {
+import { StatCardInterface, Breadcrumb } from "@/lib/definitiions";
+import Nav from "@/ui/dashboard/Nav/Nav";
+import StatCard from "@/ui/dashboard/StatCard/StatCard";
+
+const breadcrumbs: Breadcrumb[] = [
+  {
+    label: "Home",
+    href: "/",
+    active: false,
+  },
+  {
+    label: "Dashboard",
+    href: "/dashboard",
+    active: true,
+  },
+];
+async function getData(): Promise<StatCardInterface[]> {
+  return [
+    {
+      title: "Total Invoices",
+      value: 0,
+    },
+    {
+      title: "Total Customers",
+      value: 0,
+    },
+    {
+      title: "Total Employees",
+      value: 0,
+    },
+    {
+      title: "Total Albums",
+      value: 0,
+    },
+    {
+      title: "Total Tracks",
+      value: 0,
+    },
+    {
+      title: "Total Genres",
+      value: 0,
+    },
+
+    {
+      title: "Total Playlists",
+      value: 0,
+    },
+  ];
+}
+export default async function DashboardPage() {
+  const data = await getData();
+
   return (
-    <div>DashboardPage</div>
-  )
+    <>
+      <div className="flex flex-col">
+        <Nav breadcrumbs={breadcrumbs} />
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+          {data.map((item, index) => (
+            <div
+              key={index}
+              className="flex bg-green-900  text-white border rounded-xl"
+            >
+              <StatCard item={item} />
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
 }
