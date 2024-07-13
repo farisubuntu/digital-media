4eea1b6b2fedc0bbf3175ecba1c07007964927c0
farisubuntu <farisubuntu@gmail.com>
2024-07-13 08:23:51 +0300

    asdf

---
+++ src/app/dashboard/customers/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import CustomersTable from "@/ui/dashboard/Customers/CustomersTable";
+
+export default function CustomersPage() {
+  return (
+    <>
+      <CustomersPage />
+    </>
+  );
+}
